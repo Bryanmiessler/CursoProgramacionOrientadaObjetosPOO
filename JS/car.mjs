a44741e0e0f60d9d915a4d448d2160ea65ebcd10
Bryan <bryanmiessler@gmail.com>
2023-06-26 08:13:09 -0500

    iniciando repo

---
+++ JS/car.mjs
@@ -0,0 +1,8 @@
+function Car (license, driver) {
+  this.id = null
+  this.license = license
+  this.driver = driver
+  this.passenger = null
+}
+
+export default Car;
---
+++ JS/index.mjs
@@ -0,0 +1,5 @@
+import Car from './car.mjs'
+
+const car = new Car('AMS421', 'Andres Herrera')
+
+console.log(car);
